fb282b951f0c19ef73b7ecd06e625aabebc93b56
jijini <Saworieza>
2015-02-20 14:05:35 +0300

    updates

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,6 @@
 class HomeController < ApplicationController
   def index
-  	@events = Event.all
+  	@events = Event.order('created_at DESC').limit(10)
+  	#@events = Event.paginate(page: params[:page], per_page: 5)
   end
 end
